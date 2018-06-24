a5229ec76431cedb5d733106aab62086fec8a924
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 04:37:19 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,5 +71,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.values.keys
+  holiday_hash.each do |key, value|
+    puts value
+  end
 end
