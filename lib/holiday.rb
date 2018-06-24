e550bf95cb4ef369fe9669e6126a3d0246e6b34f
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 04:11:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
     puts "#{key.to_s.capitalize}:"
     value.each do |event, features|
-      result = "#{event.to_s.capitalize}:"
+      result = "\t#{event.to_s.capitalize}:"
       features.each_with_index {|item, index| result << (index != 0 ? ", #{item}" : " #{item}")}
       puts result
     end
