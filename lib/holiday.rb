322d33c50fbfd3a1a2f2eee64fa7c8dbb2e8e818
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-24 04:06:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,6 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
     value.each do |event, features|
       result = "#{event.to_s}:"
       features.each_with_index {|item| result << index != 0 ? ", #{item}" : " #{item}"}
+      puts result
     end
   end
 end
